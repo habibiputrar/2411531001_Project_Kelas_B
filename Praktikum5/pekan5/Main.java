defc59d0155331f7ffe512bafcf115ac6c1cc449
Habibi Putra Rizqullah <habibiiputraa786@gmail.com>
2025-10-27 21:54:40 +0700

    Praktikum5

--- Praktikum5/pekan5/Main.java
+++ Praktikum5/pekan5/Main.java
@@ -1,36 +1,37 @@
-package pekan5;
+package Pratikum5;
 
 public class Main {
     public static void main(String[] args) {
-        Mobil mobil = new Mobil("Toyota", "Avanza", 2021, "Bensin", "Automatic");
-        Bus bus = new Bus("Mercedes-Benz", "Bus Pariwisata", 2018, "Solar", "Eksekutif", 45);
-        Bus.JadwalPerjalanan jadwal = bus.new JadwalPerjalanan("Rute Jakarta – Bandung", "08:00");
-        Pesawat pesawat = new Pesawat("Garuda", "Boeing 737", 100, "Avtur", "Domestik");
 
-        System.out.println("Merk: " + mobil.getMerk());
-        System.out.println("Model: " + mobil.getModel());
-        System.out.println("Tahun Produksi: " + mobil.getTahunProduksi());
+        Mobil mobil = new Mobil("Toyota", "Avanza", 2021, null);
+        
+        mobil.tampilkanInfo();
         mobil.nyalakanMesin();
-        System.out.println("Jenis Bahan Bakar: " + mobil.jenisBahanBakar());
+        System.out.println("Bahan Bakar    :" + mobil.jenisBahanBakar());
         mobil.infoKonsumsi();
         mobil.fiturMobil();
+
         System.out.println();
 
-        System.out.println("Merk: " + bus.getMerk());
-        System.out.println("Model: " + bus.getModel());
-        System.out.println("Tahun Produksi: " + bus.getTahunProduksi());
+        Bus bus = new Bus("Mercedes–Benz", "Bus Pariwisata", 2018, null, 45);
+        bus.tampilkanInfo();
         bus.nyalakanMesin();
-        System.out.println("Jenis Bahan Bakar: " + bus.jenisBahanBakar());
+        System.out.println("Bahan Bakar    : " + bus.jenisBahanBakar());
         bus.infoKonsumsi();
-        System.out.println("Kapasitas Penumpang: " + bus.kapasitasPenumpang() + " penumpang");
+        bus.tampilkanInfo();
         bus.fiturBus();
+
+        Bus.JadwalPerjalanan jadwal = bus.new JadwalPerjalanan("Jakarta – Bandung", " 08:00");
         jadwal.tampilkanJadwal();
+        
         System.out.println();
 
-        System.out.println("Merk: " + pesawat.getMerk());
-        System.out.println("Model: " + pesawat.getModel());
-        System.out.println("Tahun Produksi: " + pesawat.getTahunProduksi());
+        Pesawat pesawat = new Pesawat("Boeing", "737 ", 2022, "Domestik", "Garuda Indonesia", 180);
+        pesawat.tampilkanInfo();
         pesawat.nyalakanMesin();
-        System.out.println("Jenis Bahan Bakar: " + pesawat.jenisBahanBakar());
+        System.out.println("Bahan Bakar    : " + pesawat.jenisBahanBakar());
+        pesawat.jenisPenerbangan();
+        System.out.println("Maskapai       : " + pesawat.namaMaskapai());
+
     }
 }
