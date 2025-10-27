ada840baae0aa239e7cea7262c647333fe638cb1
Habibi Putra Rizqullah <habibiiputraa786@gmail.com>
2025-10-27 22:00:17 +0700

    Praktikum5

--- Praktikum5/pekan5/Kendaraan.java
+++ Praktikum5/pekan5/Kendaraan.java
@@ -1,21 +1,26 @@
-package pekan5;
+package Pratikum5;
 
 public abstract class Kendaraan {
-    protected String merk;
-    protected String model;
-    protected int tahunProduksi;
+	private String merk;
+	private String model;
+	private int tahunProduksi;
+	
+public Kendaraan(String merk, String model, int tahunProduksi) {
 
-    public Kendaraan(String merk, String model, int tahunProduksi) {
-        this.merk = merk;
-        this.model = model;
-        this.tahunProduksi = tahunProduksi;
-    }
+	this.merk = merk;
+	this.model = model;
+	this.tahunProduksi = tahunProduksi;
+	}
 
-    public abstract void nyalakanMesin();
+public abstract void nyalakanMesin();
 
-    public final void tampilkanInfo() {
-        System.out.println("Merk          : " + merk);
-        System.out.println("Model         : " + model);
-        System.out.println("Tahun Produksi: " + tahunProduksi);
-    }
+
+public final void tampilkanInfo() {
+    System.out.println("Merk           : " + merk);
+    System.out.println("Model          : " + model);
+    System.out.println("Tahun Produksi : " + tahunProduksi);
 }
+}
+
+
+
