3e58c8bba2c117c1b896d9f8515b9271f2a08ddc
Habibi Putra Rizqullah <habibiiputraa786@gmail.com>
2025-10-27 21:56:45 +0700

    Praktikum5

--- Praktikum5/pekan5/Pesawat.java
+++ Praktikum5/pekan5/Pesawat.java
@@ -1,30 +1,40 @@
-package pekan5;
+package Pratikum5;
 
-public class Pesawat extends Kendaraan implements TransportasiUdara, Maskapai {
-    private String tipePesawat;
+public class Pesawat extends Kendaraan implements TransportasiUdara {
+    private String jenisPesawat;
+    private String maskapai;
 
-    public Pesawat(String merk, String model, int tahunProduksi, String tipePesawat) {
+
+    public Pesawat(String merk, String model, int tahunProduksi, String jenisPesawat, String maskapai, int kapasitasPenumpang) {
         super(merk, model, tahunProduksi);
-        this.tipePesawat = tipePesawat;
+        this.jenisPesawat = jenisPesawat;
+        this.maskapai = maskapai;
+
     }
 
     @Override
-    public void nyalakanMesin() {
-        System.out.println("Nyalakan Mesin: Bersiap lepas landas");
+    public void jenisPenerbangan() {
+        System.out.println("Jenis Penerbangan: " + jenisPesawat);
     }
 
+    @Override
+    public String namaMaskapai() {
+        return maskapai;
+    }
+
+    @Override
+    public void infoKonsumsi() {
+        System.out.println("Info Konsumsi: Konsumsi bahan bakar tergantung jarak tempuh dan ukuran pesawat");
+    }
+
+    public void nyalakanMesin() {
+        System.out.println("Nyalakan Mesin : Bersiap Lepas Landa");
+    }
+    
     @Override
     public String jenisBahanBakar() {
         return "Avtur";
     }
 
-    @Override
-    public void jenisPenerbangan() {
-        System.out.println("Jenis penerbangan komersial.");
-    }
 
-    @Override
-    public String namaMaskapai() {
-        return "Garuda";
-    }
 }
