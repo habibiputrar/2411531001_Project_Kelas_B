8fd768c5cb30a2110b011f465f34ff21b163088d
Habibi Putra Rizqullah <habibiiputraa786@gmail.com>
2025-10-27 21:56:01 +0700

    Praktikum5

--- Praktikum5/pekan5/Mobil.java
+++ Praktikum5/pekan5/Mobil.java
@@ -1,24 +1,32 @@
-package pekan5;
+package Pratikum5;
 
 public final class Mobil extends Kendaraan implements BahanBakar {
-    private String jenisTransmisi;
+ private String jenisTransmisi;
+ 
+ public Mobil(String merk, String model, int tahunProduksi, String jenisTransmisi) {
+     super(merk, model, tahunProduksi);
+     this.jenisTransmisi = jenisTransmisi;
+ }
 
-    public Mobil(String merk, String model, int tahunProduksi, String jenisTransmisi) {
-        super(merk, model, tahunProduksi);
-        this.jenisTransmisi = jenisTransmisi;
-    }
+ @Override
+ public void nyalakanMesin() {
+     System.out.println("Nyalakan Mesin : Tekan tombol start.");
+ }
 
-    @Override
-    public void nyalakanMesin() {
-        System.out.println("Menyalakan mesin mobil dengan menekan tombol start.");
-    }
+ @Override
+ public String jenisBahanBakar() {
+     return (" Bensin");
+ }
+ 
+ public void fiturMobil() {
+     System.out.println("Fitur mobil    : Memiliki AC dan audio premium.");
+ }
 
-    @Override
-    public String jenisBahanBakar() {
-        return "Bensin";
-    }
+ public void tampilkanDetailMobil() {
+     tampilkanInfo(); 
+     System.out.println("Jenis Transmisi : " + jenisTransmisi);
+     System.out.println("Jenis Bahan Bakar: " + jenisBahanBakar());
+ }
 
-    public void fiturMobil() {
-        System.out.println("Mobil dilengkapi dengan AC otomatis dan sistem navigasi GPS.");
-    }
+
 }
